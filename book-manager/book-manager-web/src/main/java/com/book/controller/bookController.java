bee5e682e3a366de79129fad394b9719a0b94176
wlm <541253785@qq.com>
2018-11-13 20:10:52 +0800

    bookcontroller

--- book-manager/book-manager-web/src/main/java/com/book/controller/bookController.java
+++ book-manager/book-manager-web/src/main/java/com/book/controller/bookController.java
@@ -1,8 +1,34 @@
 package com.book.controller;
 
+import java.awt.print.Book;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class bookController {
+	@Resource
+	private bookService bookService;
 
+	@RequestMapping("/addbook")
+	public String addbook(HttpServletRequest request, HttpServletResponse response) {
+		Book book = new Book();
+		String bname = request.getParameter(bname);
+		String author = request.getParameter(author);
+		double price = request.getParameter(price);
+		String press = request.getParameter(press);
+		book.setName(bname);
+		book.setAuthor(author);
+		book.setPrice(price);
+		book.setPress(press);
+		bookService.addBook(book);
+		return "listbook";
+	}
+
+	@RequestMapping("listbook")
+	public String listbook
 }
